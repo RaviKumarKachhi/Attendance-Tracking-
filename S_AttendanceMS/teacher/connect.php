7724d4f27d95479891ff6c3864ba74c439decaf4
Ravi Kumar Kachhi <111509186+RaviKumarKachhi@users.noreply.github.com>
2023-04-27 21:15:44 +0530

    Add files via upload

---
+++ S_AttendanceMS/teacher/connect.php
@@ -0,0 +1,14 @@
+<?php
+
+mysql_connect('localhost','root','') or die('Cannot connect to server');
+mysql_select_db('attmgsystem') or die ('Cannot found database');
+
+// error_reporting(0);
+// $servername  = "localhost";
+// $username ="root";
+// $password ="";
+// $dbname ="attmgsystem";
+
+// $conn = mysqli_connect($servername,$username,$password,$dbname);
+
+?>
